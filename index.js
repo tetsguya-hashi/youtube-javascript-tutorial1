9d965b53e4a79fd1af0aa732141acaaf6016bb7a
tetsuya-hashi <99647449+tetsguya-hashi@users.noreply.github.com>
2022-05-29 11:25:36 +0900

    functionVer fizzbuzz

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-let i;
-for (i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("Fizz Buzz");
+// let i;
+// for (i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("Fizz Buzz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// const fizzBuzz = (n) => {
+//   let i;
+//   for (i = n; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("Fizz Buzz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
+
+// fizzBuzz(1)
+const fizzBuzz2 = (i) => {
+  if (i / 15 === 0) {
+    return "Fizz Buzz";
   } else if (i % 5 === 0) {
-    console.log("Buzz");
+    return "Buzz";
   } else if (i % 3 === 0) {
-    console.log("Fizz");
+    return "Fizz";
   } else {
-    console.log(i);
+    return i;
   }
-}
+};
+const forWord = (t) => {
+  for (let x = t; x <= 100; x++) {
+    console.log(fizzBuzz2(x));
+  }
+};
+forWord(1);
